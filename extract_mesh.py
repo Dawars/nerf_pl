d0fc4b6af823f837ecd1d86104fb1664f88e36e0
Dávid Komorowicz <dawars00@gmail.com>
2023-05-06 22:44:16 +0200

    Change to sklearn and fix location

--- extract_mesh.py
+++ extract_mesh.py
@@ -1,78 +1,76 @@
+from pathlib import Path
+
 import torch
 import os
 import numpy as np
-import cv2
 import trimesh
-from PIL import Image
 from collections import defaultdict
 
-from torch.nn import Embedding
+from skimage import measure
 from tqdm import tqdm
-import mcubes
-import open3d as o3d
-from plyfile import PlyData, PlyElement
-from argparse import ArgumentParser
 
 from models.rendering import *
 from models.nerf import *
 
 from utils import load_ckpt
 
-from datasets import dataset_dict
-
 torch.backends.cudnn.benchmark = True
 
-
-def get_opts():
-    parser = ArgumentParser()
-    parser.add_argument('--root_dir', type=str,
-                        default="/mnt/hdd/3d_recon/sdfstudio/data/heritage/brandenburg_gate/",
-                        help='root directory of dataset')
-    parser.add_argument('--dataset_name', type=str, default='phototourism',
-                        help='which dataset to validate')
-    parser.add_argument('--scene_name', type=str, default='gate_test',
-                        help='scene name, used as output ply filename')
-    parser.add_argument('--img_wh', nargs="+", type=int, default=[800, 800],
-                        help='resolution (img_w, img_h) of the image')
-
-    parser.add_argument('--N_samples', type=int, default=64,
-                        help='number of samples to infer the acculmulated opacity')
-    parser.add_argument('--chunk', type=int, default=32 * 1024,
-                        help='chunk size to split the input to avoid OOM')
-    parser.add_argument('--ckpt_path', type=str,
-                        default="/mnt/hdd/3d_recon/nerfw/ckpts/nerfw_brandenburg_2_test512_1_0/epoch=19.ckpt",
-                        help='pretrained checkpoint path to load')
-
-    parser.add_argument('--N_grid', type=int, default=256,
-                        help='size of the grid on 1 side, larger=higher resolution')
-    parser.add_argument('--x_range', nargs="+", type=float, default=[-1.0, 1.0],
-                        help='x range of the object')
-    parser.add_argument('--y_range', nargs="+", type=float, default=[-1.0, 1.0],
-                        help='x range of the object')
-    parser.add_argument('--z_range', nargs="+", type=float, default=[-1.0, 1.0],
-                        help='x range of the object')
-    parser.add_argument('--sigma_threshold', type=float, default=10.0,
-                        help='threshold to consider a location is occupied')
-    parser.add_argument('--occ_threshold', type=float, default=0.2,
-                        help='''threshold to consider a vertex is occluded.
-                                larger=fewer occluded pixels''')
-
-    #### method using vertex normals ####
-    parser.add_argument('--use_vertex_normal', action="store_true",
-                        help='use vertex normals to compute color')
-    parser.add_argument('--N_importance', type=int, default=64,
-                        help='number of fine samples to infer the acculmulated opacity')
-    parser.add_argument('--near_t', type=float, default=1.0,
-                        help='the near bound factor to start the ray')
-
-    return parser.parse_args()
-
+coarse_mask = None
+mask = None
 
 radius = 4.6
 origin = np.array([0.568699, -0.0935532, 6.28958])
-scale = 9.360126495361328
+scale = 11.384292602539062
 bb = np.array([origin - radius,
                origin + radius]) / scale
+
+avg_pool_3d = torch.nn.AvgPool3d(2, stride=2)
+upsample = torch.nn.Upsample(scale_factor=2, mode="nearest")
+
+N_vocab = 1500
+encode_appearance = True
+N_a = 48
+encode_transient = True
+N_tau = 16
+beta_min = 0.03  # doesn't have effect in testing
+
+N_emb_xyz = 10
+N_emb_dir = 4
+N_samples = 128
+N_importance = 128
+use_disp = False
+
+embedding_xyz = PosEmbedding(N_emb_xyz - 1, N_emb_xyz)
+embedding_dir = PosEmbedding(N_emb_dir - 1, N_emb_dir)
+embedding_xyz.cuda()
+embedding_dir.cuda()
+embeddings = [embedding_xyz, embedding_dir]
+nerf_fine = NeRF("fine", W=512,
+                 in_channels_xyz=6 * N_emb_xyz + 3,
+                 in_channels_dir=6 * N_emb_dir + 3,
+                 encode_appearance=encode_appearance,
+                 in_channels_a=N_a,
+                 encode_transient=encode_transient,
+                 in_channels_t=N_tau,
+                 beta_min=beta_min
+                 )
+
+# define the dense grid for query
+N = 512
+xmin, xmax = bb[0, 0], bb[1, 0]
+ymin, ymax = bb[0, 1], bb[1, 1]
+zmin, zmax = bb[0, 2], bb[1, 2]
+# assert xmax-xmin == ymax-ymin == zmax-zmin, 'the ranges must have the same length!'
+
+x = np.linspace(xmin, xmax, N)
+y = np.linspace(ymin, ymax, N)
+z = np.linspace(zmin, zmax, N)
+xyz_ = torch.FloatTensor(np.stack(np.meshgrid(x, y, z), -1).reshape(-1, 3)).cuda()
+dir_ = torch.zeros_like(xyz_).cuda()
+
+
+# sigma is independent of direction, so any value here will produce the same result
 
 
 @torch.no_grad()
@@ -102,110 +100,120 @@ def f(models, embeddings, rays, N_samples, N_importance, chunk, white_back):
     return results
 
 
-if __name__ == "__main__":
-    args = get_opts()
+@torch.no_grad()
+def evaluate(points):
+    z_ = []
+    for _, pnts in enumerate(tqdm(torch.split(points, 100000, dim=0))):
+        xyz_embedded = embedding_xyz(pnts)  # (N, embed_xyz_channels)
+        dir_embedded = embedding_dir(torch.zeros_like(pnts).cuda())  # (N, embed_dir_channels)
+        a_embedded = torch.zeros(xyz_embedded.shape[0], N_a, device=xyz_embedded.device)
+        t_embedded = torch.zeros(xyz_embedded.shape[0], N_tau, device=xyz_embedded.device)
+        xyzdir_embedded = torch.cat([xyz_embedded, dir_embedded, a_embedded, t_embedded], 1)
+        z_.append(nerf_fine(xyzdir_embedded).cpu()[:, -1:])
+    z_ = torch.cat(z_, axis=0)
+    return z_
 
-    kwargs = {'root_dir': args.root_dir,
-              # 'img_wh': tuple(args.img_wh)
-              }
-    if args.dataset_name == 'llff':
-        kwargs['spheric_poses'] = True
-        kwargs['split'] = 'test'
-    else:
-        kwargs['split'] = 'train'
-        kwargs['img_downscale'] = 2
-        kwargs['use_cache'] = False
-    dataset = dataset_dict[args.dataset_name](**kwargs)
 
-    N_vocab = 1500
-    encode_appearance = True
-    N_a = 48
-    encode_transient = True
-    N_tau = 16
-    beta_min = 0.03  # doesn't have effect in testing
-
-    N_emb_xyz = 10
-    N_emb_dir = 4
-    N_samples = 128
-    N_importance = 128
-    use_disp = False
-
-    embedding_xyz = PosEmbedding(N_emb_xyz - 1, N_emb_xyz)
-    embedding_dir = PosEmbedding(N_emb_dir - 1, N_emb_dir)
-    embedding_xyz.cuda()
-    embedding_dir.cuda()
-    embeddings = [embedding_xyz, embedding_dir]
-    nerf_fine = NeRF("fine", W=512,
-                     in_channels_xyz=6 * N_emb_xyz + 3,
-                     in_channels_dir=6 * N_emb_dir + 3,
-                     encode_appearance=encode_appearance,
-                     in_channels_a=N_a,
-                     encode_transient=encode_transient,
-                     in_channels_t=N_tau,
-                     beta_min=beta_min
-                     )
-    load_ckpt(nerf_fine, args.ckpt_path, model_name='nerf_fine')
+def extract_mesh(epoch: int, variation: int, split: int):
+    global mask
+    global coarse_mask
+    checkpoint_path = Path(f"/mnt/hdd/3d_recon/nerfw/ckpts/nerfw_brandenburg_2_{variation}_{split}/epoch={epoch}.ckpt")
+    load_ckpt(nerf_fine, str(checkpoint_path), model_name='nerf_fine')
     nerf_fine.cuda().eval()
-
-    # define the dense grid for query
-    N = args.N_grid
-    xmin, xmax = bb[0, 0], bb[1, 0]
-    ymin, ymax = bb[0, 1], bb[1, 1]
-    zmin, zmax = bb[0, 2], bb[1, 2]
-    # assert xmax-xmin == ymax-ymin == zmax-zmin, 'the ranges must have the same length!'
-    x = np.linspace(xmin, xmax, N)
-    y = np.linspace(ymin, ymax, N)
-    z = np.linspace(zmin, zmax, N)
-
-    xyz_ = torch.FloatTensor(np.stack(np.meshgrid(x, y, z), -1).reshape(-1, 3)).cuda()
-    dir_ = torch.zeros_like(xyz_).cuda()
-    # sigma is independent of direction, so any value here will produce the same result
 
     # predict sigma (occupancy) for each grid location
     print('Predicting occupancy ...')
-    with torch.no_grad():
-        B = xyz_.shape[0]
-        out_chunks = []
-        for i in tqdm(range(0, B, args.chunk)):
-            xyz_embedded = embedding_xyz(xyz_[i:i + args.chunk])  # (N, embed_xyz_channels)
-            dir_embedded = embedding_dir(dir_[i:i + args.chunk])  # (N, embed_dir_channels)
-            a_embedded = torch.zeros(xyz_embedded.shape[0], N_a, device=xyz_embedded.device)
-            t_embedded = torch.zeros(xyz_embedded.shape[0], N_tau, device=xyz_embedded.device)
-            xyzdir_embedded = torch.cat([xyz_embedded, dir_embedded, a_embedded, t_embedded], 1)
-            out_chunks += [nerf_fine(xyzdir_embedded).cpu()]
-        rgbsigma = torch.cat(out_chunks, 0)
 
-    sigma = rgbsigma[:, -1].cpu().numpy()
-    sigma = np.maximum(sigma, 0).reshape(N, N, N)
+    xx, yy, zz = np.meshgrid(x, y, z, indexing="ij")
+    points = torch.tensor(np.vstack([xx.ravel(), yy.ravel(), zz.ravel()]).T, dtype=torch.float).cuda()
 
-    # perform marching cube algorithm to retrieve vertices and triangle mesh
-    print('Extracting mesh ...')
-    vertices, triangles = mcubes.marching_cubes(sigma, args.sigma_threshold)
+    # construct point pyramids
+    points = points.reshape(N, N, N, 3).permute(3, 0, 1, 2)
+    if coarse_mask is not None:
+        # breakpoint()
+        points_tmp = points.permute(1, 2, 3, 0)[None].cuda()
+        current_mask = torch.nn.functional.grid_sample(coarse_mask, points_tmp)
+        current_mask = (current_mask > 0.0).cpu().numpy()[0, 0]
+    else:
+        current_mask = None
 
-    ##### Until mesh extraction here, it is the same as the original repo. ######
+    points_pyramid = [points]
+    # ignore pyramid
+    # for _ in range(3):
+    #     points = avg_pool_3d(points[None])[0]
+    #     points_pyramid.append(points)
+    # points_pyramid = points_pyramid[::-1]
 
-    vertices_ = ((vertices) / N - (origin[[1, 0, 2]] / scale)).astype(np.float32)
-    ## invert x and y coordinates (WHY? maybe because of the marching cubes algo)
-    x_ = (ymax - ymin) * vertices_[:, 1] + ymin
-    y_ = (xmax - xmin) * vertices_[:, 0] + xmin
-    vertices_[:, 0] = x_ * scale
-    vertices_[:, 1] = y_ * scale
-    vertices_[:, 2] = ((zmax - zmin) * vertices_[:, 2] + zmin) * scale
-    vertices_.dtype = [('x', 'f4'), ('y', 'f4'), ('z', 'f4')]
+    # evalute pyramid with mask
+    threshold = 2 * (xmax - xmin) / N * 8
+    for pid, pts in enumerate(points_pyramid):
+        coarse_N = pts.shape[-1]
+        pts = pts.reshape(3, -1).permute(1, 0).contiguous()
 
-    face = np.empty(len(triangles), dtype=[('vertex_indices', 'i4', (3,))])
-    face['vertex_indices'] = triangles
-    PlyData([PlyElement.describe(vertices_[:, 0], 'vertex'),
-             PlyElement.describe(face, 'face')]).write(f'{args.scene_name}.ply')
+        if mask is None:
+            # only evaluate
+            if coarse_mask is not None:
+                pts_sdf = torch.ones_like(pts[:, 1])
+                valid_mask = (
+                        torch.nn.functional.grid_sample(coarse_mask, pts[None, None, None])[0, 0, 0, 0] > 0
+                )
+                if valid_mask.any():
+                    pts_sdf[valid_mask] = evaluate(pts[valid_mask].contiguous())
+            else:
+                pts_sdf = evaluate(pts)
+        else:
+            mask = mask.reshape(-1)
+            pts_to_eval = pts[mask]
 
-    # remove noise in the mesh by keeping only the biggest cluster
-    # print('Removing noise ...')
-    # mesh = o3d.io.read_triangle_mesh(f"{args.scene_name}.ply")
-    # idxs, count, _ = mesh.cluster_connected_triangles()
-    # max_cluster_idx = np.argmax(count)
-    # triangles_to_remove = [i for i in range(len(face)) if idxs[i] != max_cluster_idx]
-    # mesh.remove_triangles_by_index(triangles_to_remove)
-    # mesh.remove_unreferenced_vertices()
-    # print(f'Mesh has {len(mesh.vertices) / 1e6:.2f} M vertices and {len(mesh.triangles) / 1e6:.2f} M faces.')
-    #
-    # o3d.io.write_triangle_mesh(f"{args.scene_name}_smooth.ply", mesh)
+            if pts_to_eval.shape[0] > 0:
+                pts_sdf_eval = evaluate(pts_to_eval.contiguous())
+                pts_sdf[mask] = pts_sdf_eval
+            print("ratio", pts_to_eval.shape[0] / pts.shape[0])
+
+        # if pid < 3:
+        #     # update mask
+        #     mask = torch.abs(pts_sdf) < threshold
+        #     mask = mask.reshape(coarse_N, coarse_N, coarse_N)[None, None]
+        #     mask = upsample(mask.float()).bool()
+        #
+        #     pts_sdf = pts_sdf.reshape(coarse_N, coarse_N, coarse_N)[None, None]
+        #     pts_sdf = upsample(pts_sdf)
+        #     pts_sdf = pts_sdf.reshape(-1)
+
+        threshold /= 2.0
+
+    pts_sdf = pts_sdf.detach().cpu().numpy()
+
+    # skip if no surface found
+    # if current_mask is not None:
+    #     valid_z = z.reshape(N, N, N)[current_mask]
+    #     if valid_z.shape[0] <= 0 or (np.min(valid_z) > level or np.max(valid_z) < level):
+    #         break  # todo loop
+
+    for level in tqdm(range(5, 16, 2)):
+        if not (np.min(pts_sdf) > level or np.max(pts_sdf) < level):
+            out_path = checkpoint_path.with_name(f"mesh_{variation}_{split}_{epoch}_{level}.ply")
+
+            pts_sdf = pts_sdf.astype(np.float32)
+            verts, faces, normals, _ = measure.marching_cubes(
+                volume=pts_sdf.reshape(N, N, N),  # .transpose([1, 0, 2]),
+                level=level,
+                spacing=(
+                    (xmax - xmin) / (N - 1),
+                    (ymax - ymin) / (N - 1),
+                    (zmax - zmin) / (N - 1),
+                ),
+                mask=current_mask,
+            )
+            verts = verts + np.array([xmin, ymin, zmin])
+
+            meshcrop = trimesh.Trimesh(verts * scale, faces, normals)
+            meshcrop.export(str(out_path))
+
+
+if __name__ == "__main__":
+    for split in range(7):
+        for epoch in [0, 4, 9, 14, 19]:
+            for variation in [1, 5]:
+                extract_mesh(epoch, variation, split)
+                break
